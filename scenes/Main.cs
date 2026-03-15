231a5f84bf4e03b9411b9fac5924141f82ac1157
Jacob Christensen <jchristensen38@gatech.edu>
2026-03-15 10:27:13 -0500

    Lesson 8 - grid operations and tilemaplayer

--- scenes/Main.cs
+++ scenes/Main.cs
@@ -10,6 +10,9 @@ public partial class Main : Node2D
 	// A packed scene is effectively the data needed to create a scene
 	private PackedScene buildingScene;
 	private Button placeBuildingButton;
+	private TileMapLayer highlightTileMapLayer;
+
+	private Vector2? hoveredGridCell;
 
 	// Called when the node enters the scene tree for the first time.
 	// This ready method will be called after the ready method of all of its children
@@ -19,6 +22,7 @@ public partial class Main : Node2D
 		// Define the sprite as the Node we setup in Godot
 		cursor = GetNode<Sprite2D>("Cursor");
 		placeBuildingButton = GetNode<Button>("PlaceBuildingButton");
+		highlightTileMapLayer = GetNode<TileMapLayer>("HighlightTileMapLayer");
 
 		cursor.Visible = false;
 
@@ -36,7 +40,6 @@ public partial class Main : Node2D
 		}
 	}
 
-
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
@@ -44,6 +47,11 @@ public partial class Main : Node2D
 		// Global Position = position of the Node2d in the world space
 		// Position = relative position to its parent
 		cursor.GlobalPosition = gridPosition * 64;
+		if (cursor.Visible && (!hoveredGridCell.HasValue || hoveredGridCell.Value != gridPosition))
+		{
+			hoveredGridCell = gridPosition;
+			UpdateHighlightTileMapLayer();
+		}
 	}
 
 	private Vector2 GetMouseGridCellPosition()
@@ -61,6 +69,31 @@ public partial class Main : Node2D
 
 		var gridPosition = GetMouseGridCellPosition();
 		building.GlobalPosition = gridPosition * 64;
+		// Reset hover when clicked and reset tilemap
+		hoveredGridCell = null;
+		UpdateHighlightTileMapLayer();
+	}
+
+	private void UpdateHighlightTileMapLayer()
+	{
+		// Clear all tiles in the tilemap when the cursor moves before re-painting
+		highlightTileMapLayer.Clear();
+		// Safety check that the hovered cell actually has a value
+		if (!hoveredGridCell.HasValue)
+		{
+			return;
+		}
+
+		for (var x = hoveredGridCell.Value.X - 3; x <= hoveredGridCell.Value.X + 3; x++)
+		{
+			for (var y = hoveredGridCell.Value.Y - 3; y <= hoveredGridCell.Value.Y + 3; y++)
+			{
+				highlightTileMapLayer.SetCell(new Vector2I((int)x, (int)y), 0, Vector2I.Zero);
+			}
+		}
+		
+		
+
 	}
 
 	private void OnButtonPressed()
